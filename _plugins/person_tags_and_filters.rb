847e3159cc22c05b21467de24867dc1dadcbffd9
Brian Jones <brian@foamgarden.com>
2015-11-18 22:54:45 +0100

    Add author bio to list footers

--- _plugins/person_tags_and_filters.rb
+++ _plugins/person_tags_and_filters.rb
@@ -1,5 +1,21 @@
 module Jekyll
 
+  module AuthorBioFilter
+    def author_bio(input)
+      person = Person.new(input.strip, @context)
+
+      if person
+        name = "#{person.first_name} #{person.last_name}"
+        bio = person.bio.gsub(name, "**#{name}**")
+        result = Kramdown::Document.new(bio).to_html()
+      else
+        result = "<p>ERROR: There is no person associated with the specified id ‘#{input.strip}’.</p>"
+      end
+
+      result
+    end
+  end
+
   module PersonFullNameFilter
     def person_full_name(input)
       person = Person.new(input.strip, @context)
@@ -15,4 +31,5 @@ module Jekyll
   end
 end
 
+Liquid::Template.register_filter(Jekyll::AuthorBioFilter)
 Liquid::Template.register_filter(Jekyll::PersonFullNameFilter)
