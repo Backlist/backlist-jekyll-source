480f9068a2ac6528a6616485764bcf49ba57c409
Brian Jones <brian@foamgarden.com>
2015-11-28 07:00:58 +0100

    Roughed implementation of homepage recommendations

--- _plugins/misc_generators.rb
+++ _plugins/misc_generators.rb
@@ -1,6 +1,53 @@
 require 'json'
 
 module Jekyll
+  class HomepageRandomBookDataGenerator < Generator
+    safe true
+
+    def generate(site)
+      lists = []
+      books = []
+
+      site.posts.docs.each do |post|
+        if post.data['layout'] == 'list'
+          lists << {
+              id: post.data['id'],
+              title: post.data['title'],
+              permalink: post.data['permalink'] }
+        end
+      end
+
+      # TODO: Refactor to avoid nested loops
+      site.collections['books'].docs.each do |book|
+        if defined? book.data['has_cover_image'] and book.data['has_cover_image']
+          if defined? book.data['reviews']
+            list_title = ''
+            list_permalink = ''
+            book.data['reviews'].each do |review|
+              lists.each do |list|
+                if list[:id] == review['list_id']
+                  list_title = list[:title]
+                  list_permalink = list[:permalink]
+                  break
+                end
+              end
+              books << {
+                id: book.data['id'],
+                list_title: list_title,
+                list_permalink: list_permalink
+              }
+            end
+          end
+        end
+      end
+
+      f = File.new(File.join(site.source, 'data-includes/homepage-featured-book-source.json'), 'w+')
+      f.write(JSON.generate(books))
+      f.close
+      site.static_files << Jekyll::StaticFile.new(site, site.source, 'data-includes', 'homepage-featured-book-source.json')
+    end
+  end
+
   class Error404PageLinkDataGenerator < Generator
     safe true
 
